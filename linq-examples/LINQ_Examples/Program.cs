cb03d8c418130ec58f95d6266aecf06fb052385c
Genesis Rivera <genesisrrios@gmail.com>
2020-09-02 23:48:56 -0400

    Commiting new linq project

---
+++ linq-examples/LINQ_Examples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First query example
+            var customerList = new List<string>
+            {
+                "Cliente 1",
+                "Cliente 2",
+                "Cliente 3"
+            };
+            var firstCustomer = customerList.Where(x => x == "Mofongo").SingleOrDefault();
+        }
+    }
+}
