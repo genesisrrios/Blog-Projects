b73a24abdc30a92caf4558d6930349c5364e3aeb
grivera <grivera@multisystems.com>
2020-12-21 22:21:36 -0400

    Added pancake class to show stacks and explained a bit about lists and arrays.

---
+++ c-sharp-data-structures/CSharpDataStructures/Pancake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpDataStructures
+{
+    public class Pancake
+    {
+        public enum SizeEnum
+        {
+            TooSmall,
+            StillTooSmall,
+            HugeAndPerfect
+        }
+        public string Name { get; set; }
+        public string Syrup { get; set; }
+        public SizeEnum Size { get; set; }
+    }
+}
--- c-sharp-data-structures/CSharpDataStructures/Program.cs
+++ c-sharp-data-structures/CSharpDataStructures/Program.cs
@@ -11,6 +11,7 @@ namespace CSharpDataStructures
         static void Main(string[] args)
         {
             //FIFO !
+            //////////////////////////////QUEUE///////////////////////////////////
             var queue = new Queue<Artist>();
             queue.Enqueue(new Artist
             {
@@ -34,9 +35,73 @@ namespace CSharpDataStructures
             queue.Dequeue();
             foreach (var artist in queue)
             {
-                Console.WriteLine($"Name: {artist.Name}, Genre: {artist.GenreId}");
+                //Console.WriteLine($"Name: {artist.Name}, Genre: {artist.GenreId}");
             }
-            Console.Read();
+            //Console.ReadKey();
+
+            ///////////////////////////////ARRAY/////////////////////////////////
+
+            var artistArray = new Artist[4];
+
+            artistArray[0] = new Artist
+            {
+                Name = "Hayley kioko",
+                GenreId = "Pop",
+                RecordId = 4
+            };
+
+            artistArray[1] = new Artist
+            {
+                Name = "Glass animals",
+                GenreId = "Vaporwave idk",
+                RecordId = 5
+            };
+            //Error line
+            //artistArray[5] = new Artist
+            //{
+            //    Name = "Error",
+            //    GenreId = "This shouldn't work",
+            //    RecordId = 5
+            //};
+            foreach (var artist in artistArray)
+            {
+                if (artist == default) continue;
+                //Console.WriteLine(artist?.Name);
+            }
+
+            var hayleyKioko = artistArray[0];
+            //Console.WriteLine(hayleyKioko.Name);
+
+            //Console.ReadKey();
+
+            /////////////////////LIST///////////////
+            var artistList = new List<Artist>();
+            artistList.AddRange(artistArray);
+            //artistList.ForEach(x => Console.WriteLine(x?.Name));
+            //Console.ReadKey();
+            var halestorm = new Artist
+            {
+                Name = "Halestorm",
+                GenreId = "Rock",
+                RecordId = 6
+            };
+            artistList.Add(halestorm);
+            Console.WriteLine($"Artist list count: {artistList.Count}");
+            Console.WriteLine($"Does the list contain halestorm? : {artistList.Contains(halestorm)}");
+            Console.WriteLine($"Find by RecordId : {artistList.Find(x=> x?.RecordId == 6).Name}");
+            Console.ReadLine();
+
+
+            var stack = new Stack<Pancake>();
+            stack.Push(new Pancake
+            {
+                Name ="Chocolate chips",
+                Size = Pancake.SizeEnum.TooSmall,
+                Syrup = "Maple Syrup"
+            });
+            stack.Pop();
+            stack.Peek();
+            
         }
     }
 }
