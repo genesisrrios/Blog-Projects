5dbbb0bae0972a1159aefe2114c8549f6d563804
grivera <grivera@multisystems.com>
2021-02-20 13:03:12 -0400

    Adding new project.

---
+++ asp-net-core-vue-js/AspNetCoreVueJs/AspNetCoreVueJs/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using AspNetCoreVueJs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace AspNetCoreVueJs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+		private const string TempDataFriendsList = "FriendsList";
+
+		public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+		public IActionResult Index()
+		{
+			var friendsList = new List<User>
+				{
+					new User
+					{
+						LastName = "Ford",
+						Name = "Henry",
+						Username = "henryford121"
+					},
+					new User
+					{
+						LastName = "Montgo",
+						Name = "Suzzie",
+						Username = "mongosus"
+					},
+					new User
+					{
+						LastName = "Rivera",
+						Name = "Luis",
+						Username = "starraiden"
+					} 
+			};
+			TempData[TempDataFriendsList] = friendsList;
+			var model = new IndexViewModel
+			{
+				User = new User
+				{
+					LastName = "Rivera",
+					Name = "Genesis",
+					Username = "genesisrrios"
+				},
+				FriendList = friendsList
+			};
+			return View(model);
+		}
+		[HttpPost]
+        public bool InsertNewFriendInMemory([FromBody]User friend)
+        {
+        if (friend == default || !TempData.ContainsKey(TempDataFriendsList)) return false;
+			var friendsList = (List<User>)TempData[TempDataFriendsList];
+			friendsList.Add(friend);
+			TempData[TempDataFriendsList] = friendsList;
+            return true;
+        }
+        public List<User> GetFriendsList()
+        {
+            return (List<User>)TempData[TempDataFriendsList];
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ asp-net-core-vue-js/AspNetCoreVueJs/AspNetCoreVueJs/Models/IndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVueJs.Models
+{
+    public record User
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+    public record IndexViewModel
+    {
+        public User User { get; set; }
+
+        public List<User> FriendList { get; set; }
+    }
+}
